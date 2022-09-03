fdf4c49a7a0e53c31a1cc9a747c9a15e611841dd
HAGCB <112761910+HAGCB@users.noreply.github.com>
2022-09-03 21:30:20 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,23 +4,23 @@ export const config = {
    */
 
   // 公众号APP_ID
-  APP_ID: "",
+  APP_ID: "wxc8a62ef46d0d9d51",
 
   // 公众号APP_SECRET
-  APP_SECRET: "",
+  APP_SECRET: "71a865ff5110eaa72dc0f769b626e4eb",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
    * [
    *  {
    *    // 想要发送的人的名字
-   *    name: "老婆0",
+   *    name: "程砚霸总",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *    id: "o6Jhv5y4AR1gbsIhYR2THnpCdj9g",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "3lWuoHNRW7-AxeI9Sv0gK1HRblZ1JIDszEngkeJldkQ",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
+   *    openUrl: "https://bkpp.cn"
    *   }, 
    * ]
    */
@@ -60,15 +60,15 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjVglIuCXo34CsraXWEio1i3P7sZJ7IQEES93lDsgc4yOU",
+  CALLBACK_TEMPLATE_ID: "3lWuoHNRW7-AxeI9Sv0gK1HRblZ1JIDszEngkeJldkQ",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: "自己",
+      name: "余总",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO36VskY3v0Kuf-ywIZVR3MpuXhk",
+      id: "o6Jhv57T2EfTFYUb2nOc0t6u2S1E",
     }, 
   ],
     
@@ -79,9 +79,9 @@ export const config = {
   /** 天气相关 */
 
   // 所在省份
-  PROVINCE: "广东",
+  PROVINCE: "安徽",
   // 所在城市
-  CITY: "惠州",
+  CITY: "合肥",
 
   /** 重要节日相关 */
 
@@ -92,10 +92,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+    {"type": "生日", "name": "程总", "year": "2002", "date": "07-27"},
+    {"type": "节日", "name": "知遇日", "year": "2017", "date": "09-13"},
+    {"type": "生日", "name": "余总", "year": "2002", "date": "06-22"},
+    {"type": "节日", "name": "元旦", "year": "2023", "date": "01-01"},
   ],
 
   /**
